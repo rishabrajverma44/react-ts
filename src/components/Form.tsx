5c399f2b0713879fc00e4e3bc8e4bc21ee11276d
rishab raj verma <rishabrajverma44@gmail.com>
2025-07-25 10:49:30 +0530

    feat : add handler functions to track values

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,27 +1,66 @@
+import type React from "react";
 import styles from "../Style/form.module.css";
+import type { formInterFace } from "../types/types";
 import { useState } from "react";
 
+const defaultForm: formInterFace = {
+  id: null,
+  company: "",
+  role: "",
+  jobtype: "",
+  location: "",
+  status: "",
+  date: "",
+  notes: "",
+};
+
 const Form = () => {
-  const handleSubmit = () => {
+  const [form, setForm] = useState<formInterFace>(defaultForm);
+
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
+  ) => {
+    const { name, value } = e.target;
+    setForm((pervious) => ({ ...pervious, [name]: value }));
+    console.log(name);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
     console.log("submit");
   };
 
   return (
     <div>
-      <form
-        onSubmit={(e: React.SyntheticEvent) => {
-          e.preventDefault();
-          handleSubmit();
-        }}>
+      <form onSubmit={handleSubmit}>
         <div id={styles.applicationForm}>
           <label>Company Name:</label>
-          <input type="text" placeholder="Company name" />
+          <input
+            type="text"
+            placeholder="Company name"
+            name="company"
+            onChange={handleChange}
+            value={form.company}
+          />
 
           <label>Role:</label>
-          <input type="text" id="role" placeholder="Enter role" />
+          <input
+            type="text"
+            id="role"
+            name="role"
+            placeholder="Enter role"
+            value={form.role}
+            onChange={handleChange}
+          />
 
           <label>Job Type:</label>
-          <select id="jobType">
+          <select
+            id="jobType"
+            name="jobtype"
+            onChange={handleChange}
+            value={form.jobtype}>
             <option value="" disabled>
               Select job type
             </option>
@@ -31,13 +70,31 @@ const Form = () => {
           </select>
 
           <label id="locationLabel">Location:</label>
-          <input type="text" id="location" placeholder="Enter location" />
+          <input
+            type="text"
+            id="location"
+            name="location"
+            placeholder="Enter location"
+            value={form.location}
+            onChange={handleChange}
+          />
 
           <label>Application Date:</label>
-          <input type="date" id="date" />
+          <input
+            type="date"
+            id="date"
+            name="date"
+            onChange={handleChange}
+            value={form.date}
+          />
 
           <label>Application Status:</label>
-          <select id="status" className="form-control">
+          <select
+            id="status"
+            className="form-control"
+            name="status"
+            onChange={handleChange}
+            value={form.status}>
             <option value="" disabled>
               Select status
             </option>
@@ -48,7 +105,11 @@ const Form = () => {
           </select>
 
           <label>Notes:</label>
-          <textarea id="notes"></textarea>
+          <textarea
+            id="notes"
+            name="notes"
+            value={form.notes}
+            onChange={handleChange}></textarea>
 
           <button type="submit" id="submitBtn">
             Add Application
---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export interface formInterFace {
+  id: null | string;
+  company: string;
+  role: string;
+  jobtype: string;
+  location: string;
+  status:string,
+  date: string;
+  notes: string;
+}
