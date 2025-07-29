cfd177076a6fe9364d6fa9f2d3254ca4518602c6
rishab raj verma <rishabrajverma44@gmail.com>
2025-07-29 18:38:29 +0530

    feat : pkg install, filter html

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,44 +1,11 @@
-import type React from "react";
 import styles from "../Style/search.module.css";
-import { useState } from "react";
 
 const Filter = () => {
-  const [searchValue, setSearch] = useState("");
-  function searchFunction() {
-    console.log("searching...", searchValue);
-  }
-  // function debounce(fun: Function) {
-  //   let timeout: number;
-  //   return function (...args: string[]) {
-  //     clearTimeout(timeout);
-  //     timeout = setTimeout(() => {
-  //       fun(...args);
-  //     }, 4000);
-  //   };
-  // }
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    const searchQuery = e.target.value.trim();
-    if (searchQuery.length > 0) {
-      searchFunction();
-    }
-  };
-  const handleClear = () => {
-    setSearch("");
-  };
   return (
     <div>
       <div className={styles.search_box}>
-        <input
-          className={styles.searchFileld}
-          type="input"
-          placeholder="Search "
-          onChange={handleChange}
-          value={searchValue}
-        />
-        <button className={styles.clear} onClick={handleClear}>
-          X
-        </button>
+        <input className={styles.searchFileld} type="input" placeholder="Search " />
+        <button className={styles.clear}>X</button>
       </div>
     </div>
   );
