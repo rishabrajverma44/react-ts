28235691ef3c1a58a37951bb32896e5a962f7fe0
rishab raj verma <rishabrajverma44@gmail.com>
2025-08-08 00:14:08 +0530

    feat : api integration of company

--- src/formValidation/validation.ts
+++ src/formValidation/validation.ts
@@ -1,4 +1,13 @@
 import type { formInterface } from "../types";
+//date compare
+const dateValidator = (formDate: Date) => {
+  const inputDate = formDate;
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  inputDate.setHours(0, 0, 0, 0);
+
+  return inputDate < today;
+};
 export function validationForForm(form: formInterface) {
   //partrial utility type used for giving types of an object wich accepts (key, type)
   const errors: Partial<Record<keyof formInterface, string>> = {};
@@ -9,6 +18,8 @@ export function validationForForm(form: formInterface) {
   if (form.jobType !== "Remote" && !form.location.trim())
     errors.location = "Location is required !";
   if (!form.date.trim()) errors.date = "Date is required !";
+  if (dateValidator(new Date(form.date)))
+    errors.date = "Date cannot be in the past!";
   if (!form.status.trim()) errors.status = "Status is required !";
   return errors;
 }
@@ -52,6 +63,8 @@ export function validationForSingleField(
     case "date":
       if (!form.date.trim()) {
         errors.date = "Date is required !";
+      } else if (dateValidator(new Date(form.date))) {
+        errors.date = "Date cannot be in the past!";
       } else {
         errors.date = "";
       }
